9426f8ad01bc2cbdb9da2d80068bf523ceded3f0
Alexander <kazakov_ai87@mail.ru>
2021-01-25 13:58:01 +0300

    'init'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, {useState} from 'react'
 
 function App() {
+  const [userText, setUserText] = useState('')
+  const [result, setResult] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const classNames = ['red', 'green', 'blue', 'orange']
+    const array = userText.split(' ')
+    const tempArray = array.map((word, index) => {
+      let className = classNames[Math.floor(Math.random() * classNames.length)];
+      return <span className={className} key={index}>{word} </span>
+    })
+    setUserText(tempArray)
+    setResult(true)
+  }
+  const handleChange = (e) => {
+    setUserText(e.currentTarget.textContent)
+  }
+  const handleClear = () => {
+    setUserText('')
+    setResult(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form action="" onSubmit={handleSubmit}>
+        <div
+          id="textarea"
+          className={`textarea ${result ? 'hidden' : ''}`}
+          onInput={handleChange}
+          contentEditable
+        />
+        <div
+          className={`textarea ${result ? '' : 'hidden'}`}
         >
-          Learn React
-        </a>
-      </header>
+          {userText}
+        </div>
+        <button className="button" type="submit">Анализировать</button>
+        <button className="button" onClick={handleClear}>Очистить</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
